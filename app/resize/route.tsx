2df44e26b5d32c52c9a029724a11ba3936977036
Mark Sergienko <ffab00@gmail.com>
2023-10-09 13:20:26 +0100

    first commit

---
+++ app/resize/route.tsx
@@ -0,0 +1,25 @@
+import { ImageResponse } from "next/server";
+import sharp from "sharp";
+
+export const runtime = "edge";
+
+export default async function handler(req) {
+  const { searchParams } = new URL(req.url);
+  const src = searchParams.get("src");
+  const width = parseInt(searchParams.get("width"));
+  const quality = parseInt(searchParams.get("quality"));
+
+  const imageBuffer = await fetch(src).then((res) => res.arrayBuffer());
+  const resizedImageBuffer = await sharp(Buffer.from(imageBuffer))
+    .resize(width)
+    .jpeg({ quality })
+    .toBuffer();
+
+  return new ImageResponse(resizedImageBuffer, {
+    ttl: 60 * 60 * 24, // 24 hours
+    status: 200,
+    headers: {
+      "Content-Type": "image/jpeg",
+    },
+  });
+}
